877c21d3871b3fa5d737254d3f0e44c1e37f1686
felixpotie <felixpotie@gmail.com>
2020-10-06 15:22:17 +0200

    first commit + migration db

---
+++ giveactions/campagne/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    nom = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nom
+
+
+class Campagne(models.Model):
+    titre = models.CharField(max_length=50)
+    description = models.CharField(max_length=600)
+    image = models.URLField()
+    tags = models.ManyToManyField(Tag, related_name='tags', blank=True)
+
+    def __str__(self):
+        return self.titre
+
---
+++ giveactions/campagne/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Campagne, Tag
+
+
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('nom')
+
+class CampagneSerializer(serializers.HyperlinkedModelSerializer):
+    tags = TagSerializer(read_only=True, many=True)
+    class Meta:
+        model = Campagne
+        fields = ('titre', 'description', 'image', 'tags')
