88ed24c53f303d531915ce7e87fca2b808bbec86
felixpotie <felixpotie@gmail.com>
2020-10-09 11:10:22 +0200

    change query by tag for some routes

--- giveactions/campaign/views.py
+++ giveactions/campaign/views.py
@@ -12,42 +12,42 @@ class CampaignViewSet(viewsets.ModelViewSet):
 
     @action(detail=False)
     def byTag(self, request):
-        query = request.GET.get('query')
-        campaigns = Campaign.objects.filter(tags__id=query)
+        tag = request.GET.get('tag')
+        campaigns = Campaign.objects.filter(tags__id=tag)
         serializer = CampaignSerializer(campaigns, many=True)
         return Response(serializer.data)
     
 
     @action(detail=True, methods=['put'])
     def addTag(self, request, pk=None):
-        query = request.GET.get('query')
+        tagId = request.GET.get('tag')
         campaign = self.get_object()
         try:
-            tag = Tag.objects.get(pk=query)
-            if Campaign.objects.filter(tags__id=query).filter(id=campaign.id).exists():
+            tag = Tag.objects.get(pk=tagId)
+            if Campaign.objects.filter(tags__id=tagId).filter(id=campaign.id).exists():
                 return Response({'message': 'no change, tag '+str(tag)+' is already linked to the campaign '+str(campaign)})
             else:
                 campaign.tags.add(tag)
                 serializer = CampaignSerializer(campaign)
                 return Response(serializer.data)
         except ObjectDoesNotExist:
-            return Response({'message': 'tag n°'+query+' does not exists'}, status=404)
+            return Response({'message': 'tag n°'+str(tagId)+' does not exists'}, status=404)
         
 
     @action(detail=True, methods=['put'])
     def deleteTag(self, request, pk=None):
-        query = request.GET.get('query')
+        tagId = request.GET.get('tag')
         campaign = self.get_object()
         try:
-            tag = Tag.objects.get(pk=query)
-            if Campaign.objects.filter(tags__id=query).filter(id=campaign.id).exists():
+            tag = Tag.objects.get(pk=tagId)
+            if Campaign.objects.filter(tags__id=tagId).filter(id=campaign.id).exists():
                 campaign.tags.remove(tag)
                 serializer = CampaignSerializer(campaign)
                 return Response(serializer.data)
             else:
                 return Response({'message': 'tag '+str(tag)+' is not linked to the campaign '+str(campaign)}, status=404)
         except ObjectDoesNotExist:
-            return Response({'message': 'tag n°'+query+' does not exists'}, status=404)
+            return Response({'message': 'tag n°'+str(tagId)+' does not exists'}, status=404)
                 
 
 
